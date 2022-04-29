719df3978be00a07260fbd751ee22e656c8bde92
Alt-Shivam <shivank1128@gmail.com>
2022-04-29 22:39:44 +0530

    minor changes

--- Script/net.sh
+++ Script/net.sh
@@ -3,6 +3,6 @@ set -xe
 function net_default_iface {
  sudo ip -4 route list 0/0 | awk '{ print $5; exit }'
 }
-cat > /opt/openstack-helm-infra/tools/gate/devel/multinode-vars.yaml <<EOF
+cat > /home/ubuntu/openstack-helm-infra/tools/gate/devel/multinode-vars.yaml <<EOF
 kubernetes_network_default_device: $(net_default_iface)
 EOF
